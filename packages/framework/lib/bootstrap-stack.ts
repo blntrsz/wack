f51742671b991a1adbb0b7d4e43e36fda4ac3880
Orosz Bálint <balint@Orosz-MacBook-Pro.local>
2024-06-06 21:59:29 +0200

    add bootstrap with s3 bucket

--- packages/framework/lib/bootstrap-stack.ts
+++ packages/framework/lib/bootstrap-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Bucket, EventType } from "aws-cdk-lib/aws-s3";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { S3EventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import { DockerImageCode, DockerImageFunction } from "aws-cdk-lib/aws-lambda";
+import { Platform } from "aws-cdk-lib/aws-ecr-assets";
 import { join } from "path";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class BootstrapStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,9 +15,26 @@ export class BootstrapStack extends cdk.Stack {
       bucketName: "asgard-main-bucket",
     });
 
-    const lambda = new NodejsFunction(this, "bootstrap", {
-      entry: join(import.meta.dirname, "bootstrap.ts"),
+    const lambda = new DockerImageFunction(this, "docker-image", {
+      code: DockerImageCode.fromImageAsset(
+        join(import.meta.dirname, "bootstrap"),
+        {
+          platform: Platform.LINUX_AMD64,
+        }
+      ),
+      functionName: "bootstrap",
+      memorySize: 512,
+      timeout: cdk.Duration.minutes(15),
     });
+    bucket.grantRead(lambda);
+
+    lambda.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: ["*"],
+        actions: ["*"],
+      })
+    );
 
     lambda.addEventSource(
       new S3EventSource(bucket, {
--- packages/framework/lib/bootstrap.ts
+++ packages/framework/lib/bootstrap.ts
@@ -1,5 +1,75 @@
-export function handler(event: any, context: any) {
-  console.log(JSON.stringify(event, null, 2));
-  console.log(JSON.stringify(context, null, 2));
+import { GetObjectCommand, S3 } from "@aws-sdk/client-s3";
+import { exec } from "child_process";
+import fsp from "fs/promises";
+import decompress from "decompress";
+
+const client = new S3();
+
+interface Event {
+  Records: {
+    eventVersion: string;
+    eventSource: string;
+    awsRegion: string;
+    eventTime: string;
+    eventName: string;
+    userIdentity: {
+      principalId: string;
+    };
+    requestParameters: {
+      sourceIPAddress: string;
+    };
+    responseElements: {
+      "x-amz-request-id": string;
+      "x-amz-id-2": string;
+    };
+    s3: {
+      s3SchemaVersion: string;
+      configurationId: string;
+      bucket: {
+        name: string;
+        ownerIdentity: {
+          principalId: string;
+        };
+        arn: string;
+      };
+      object: {
+        key: string;
+        sequencer: string;
+      };
+    };
+  }[];
+}
+
+export async function handler(event: Event) {
+  for (const record of event.Records) {
+    console.log({
+      Bucket: record.s3.bucket.name,
+      Key: record.s3.object.key,
+    });
+    const obj = await client.send(
+      new GetObjectCommand({
+        Bucket: record.s3.bucket.name,
+        Key: record.s3.object.key,
+      })
+    );
+
+    if (!obj) {
+      throw new Error("Impossible state");
+    }
+
+    const body = (await obj.Body?.transformToString()) ?? "";
+
+    await fsp.writeFile(`/tmp/${record.s3.object.key}`, body);
+
+    await decompress(
+      `/tmp/${record.s3.object.key}`,
+      `/tmp/${record.s3.object.key}`.replace(".zip", "")
+    );
+
+    exec(`ls -la /tmp`, (_err, stout, _sterr) => {
+      console.log(stout);
+    });
+  }
+
   return {};
 }
