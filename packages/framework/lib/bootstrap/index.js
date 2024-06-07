dbbb89362f949459b3c758ba836c1c9a363db833
Orosz Bálint <balint@Orosz-MacBook-Pro.local>
2024-06-07 18:58:57 +0200

    add delete branch ci

--- packages/framework/lib/bootstrap/index.js
+++ packages/framework/lib/bootstrap/index.js
@@ -7,6 +7,31 @@ import { promisify } from "util";
 const client = new S3();
 const pipe = promisify(pipeline);
 
+async function createPipeline(record) {
+  const obj = await client.send(
+    new GetObjectCommand({
+      Bucket: record.s3.bucket.name,
+      Key: record.s3.object.key,
+    })
+  );
+
+  if (!obj) {
+    throw new Error("Impossible state");
+  }
+
+  const branch = record.s3.object.key.replace(".zip", "");
+
+  const writeStream = fs.createWriteStream(`/tmp/${record.s3.object.key}`);
+  await pipe(obj.Body, writeStream);
+  console.log("Write finished");
+
+  await $`unzip -o /tmp/${record.s3.object.key} -d /tmp/${branch}`;
+  console.log("Unzipping finished");
+
+  await $`npx cdk deploy --app '/tmp/${branch}/packages/infra/cdk.out/' pipeline --require-approval=never`;
+  console.log("Deployed");
+}
+
 export async function handler(event) {
   console.debug(JSON.stringify(event));
 
@@ -15,27 +40,9 @@ export async function handler(event) {
       Bucket: record.s3.bucket.name,
       Key: record.s3.object.key,
     });
-    const obj = await client.send(
-      new GetObjectCommand({
-        Bucket: record.s3.bucket.name,
-        Key: record.s3.object.key,
-      })
-    );
 
-    if (!obj) {
-      throw new Error("Impossible state");
+    if (record.eventName === "ObjectCreated:Put") {
+      createPipeline(record);
     }
-
-    const branch = record.s3.object.key.replace(".zip", "");
-
-    const writeStream = fs.createWriteStream(`/tmp/${record.s3.object.key}`);
-    await pipe(obj.Body, writeStream);
-    console.log("Write finished");
-
-    await $`unzip -o /tmp/${record.s3.object.key} -d /tmp/${branch}`;
-    console.log("Unzipping finished");
-
-    await $`npx cdk deploy --app '/tmp/${branch}/packages/infra/cdk.out/' pipeline --require-approval=never`;
-    console.log("Deployed");
   }
 }
