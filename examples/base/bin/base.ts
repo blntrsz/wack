01b15d933b3b57aa38eec2e61e19735a9756917c
Balint Orosz <orosz.balint.97@gmail.com>
2024-06-22 18:35:57 +0200

    initial commit

---
+++ examples/base/bin/base.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import { BaseStack } from '../lib/base-stack';
+import { CDKApplication } from 'opinionated-ci-pipeline';
+
+new CDKApplication({
+  stacks: {
+    create: (scope, projectName, envName) => {
+      new BaseStack(scope, 'BaseStack', { stackName: `${projectName}-${envName}-BaseStack` });
+    },
+  },
+  repository: {
+    host: 'github',
+    name: 'blntrsz/asgard',
+  },
+  packageManager: 'pnpm',
+  commands: {
+    preInstall: ["cd examples/base"],
+  },
+  cdkOutputDirectory: "examples/base/cdk.out",
+  pipeline: [
+    {
+      environment: 'test',
+      post: [
+        'echo "do integration tests here"',
+      ],
+    },
+    {
+      environment: 'prod',
+    },
+  ],
+});
